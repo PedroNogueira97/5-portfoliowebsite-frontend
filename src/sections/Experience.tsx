b04cb5b972d4b5d787b6d6e34656a3e06cf76f60
Pedro Nogueira <pedronogueiramoraes@hotmail.com>
2026-04-27 11:11:16 -0300

    Fix: Remove Link import never used

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
 import { Briefcase } from 'lucide-react';
-import { Link } from 'react-router-dom';
 
 export default function Experience() {
     const { t } = useTranslation();
